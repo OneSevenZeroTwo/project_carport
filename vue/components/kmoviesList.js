9ec994616ac86bf9aa0faac30c7d44eac7a73128
kell90722 <2531801933@qq.com>
2017-07-29 18:22:10 +0800

    listnav

--- vue/components/kmoviesList.js
+++ vue/components/kmoviesList.js
@@ -1,6 +1,7 @@
 Vue.component("kmoviesList", {
 	template: `
-		<div id="pullrefresh" class="mui-content mui-scroll-wrapper">
+		<div id="pullrefresh">
+
 			<div class="title">
 				Top100电影榜
 			</div>
@@ -22,90 +23,62 @@ Vue.component("kmoviesList", {
 			</div>
 		</div>
 	`,
+	data() {
+		return {
+			count: 10
+		}
+	},
 	methods: {
-		movieLoad(){
+		movieLoad() {
 			this.$store.dispatch("testJsonp")
 		},
 		movieList() {
-			Vue.http.jsonp("https://api.douban.com//v2/movie/top250",{
-						params:{
-							count:10
-						}
-				}).then(function(data) {
-					this.store.state.movieInfo = data.body.subjects
-//					console.log(state.movieInfo)
-				}, function(err) {
-					console.log(err)
-				})
+//			console.log(this)
+			this.count += 10
+			Vue.http.jsonp("https://api.douban.com//v2/movie/top250", {
+				params: {
+					count: this.count
+				}
+			}).then(function(data) {
+				this.store.state.movieInfo = data.body.subjects
+				//					console.log(state.movieInfo)
+			}, function(err) {
+				console.log(err)
+			})
 		}
 	},
-	mounted(){
+	mounted() {
 		this.movieLoad();
 		this.movieList();
-		mui.init({
-				pullRefresh: {
-					container: '#pullrefresh',
-					down: {
-						callback: pulldownRefresh
-					},
-					up: {
-						contentrefresh: '正在加载...',
-						callback: pullupRefresh
-					}
-				}
-			});
-			/**
-			 * 下拉刷新具体业务实现
-			 */
-			function pulldownRefresh() {
-				setTimeout(function() {
-					var table = document.body.querySelector('.mui-table-view');
-					var cells = document.body.querySelectorAll('.mui-table-view-cell');
-					for (var i = cells.length, len = i + 3; i < len; i++) {
-						var li = document.createElement('li');
-						li.className = 'mui-table-view-cell';
-						li.innerHTML = '<a class="mui-navigate-right">Item ' + (i + 1) + '</a>';
-						//下拉刷新，新纪录插到最前面；
-						table.insertBefore(li, table.firstChild);
-					}
-					mui('#pullrefresh').pullRefresh().endPulldownToRefresh(); //refresh completed
-				}, 1500);
-			}
-			var count = 0;
-
-			function pullupRefresh() {
-				setTimeout(function() {
-					mui('#pullrefresh').pullRefresh().endPullupToRefresh((++count > 2)); //参数为true代表没有更多数据了。
-					var table = document.body.querySelector('.mui-table-view');
-					var cells = document.body.querySelectorAll('.mui-table-view-cell');
-					for (var i = cells.length, len = i + 20; i < len; i++) {
-						var li = document.createElement('li');
-						li.className = 'mui-table-view-cell';
-						li.innerHTML = '<a class="mui-navigate-right">Item ' + (i + 1) + '</a>';
-						table.appendChild(li);
-					}
-				}, 1500);
-			}
-			if (mui.os.plus) {
-				mui.plusReady(function() {
-					setTimeout(function() {
-						mui('#pullrefresh').pullRefresh().pullupLoading();
-					}, 1000);
-
-				});
-			} else {
-				mui.ready(function() {
-					mui('#pullrefresh').pullRefresh().pullupLoading();
-				});
-			}
+//		mui.init({
+//			pullRefresh: {
+//				container: '#pullrefresh',
+//				down: {
+//					callback: pulldownRefresh
+//				},
+//				up: {
+//					contentrefresh: '正在加载...',
+//					callback: pullupRefresh
+//				}
+//			}
+//		});
+		//		if(mui.os.plus) {
+		//			mui.plusReady(function() {
+		//				setTimeout(function() {
+		//					mui('#pullrefresh').pullRefresh().pullupLoading();
+		//				}, 1000);
+		//
+		//			});
+		//		} else {
+		//			mui.ready(function() {
+		//				mui('#pullrefresh').pullRefresh().pullupLoading();
+		//			});
+		//		}
 	},
 	computed: {
 		movieInfo() {
 			//			console.log(this.$store.state.movieInfo.images)
 			return this.$store.state.movieInfo
 		}
-	},
-	components: {
-		kclacontent: {}
 	}
 })
